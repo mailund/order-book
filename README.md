cada50f850dbe8037ffbe360b7d01ba2dee1cc2f
Thomas Mailund <tm@kvantify.dk>
2025-05-22 11:03:14 +0200

    Description and simulator

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# order-book
-Order book exercise
+# Order book exercise
+
+The task is to implement a simple orderbook. The orderbook should be able to handle a stream of order events and maintain the state of the orderbook. The orderbook should be able to provide the state of the orderbook at any given time.
+
+## Formal Definition
+
+We define a limit order as an element of
+
+```
+Order = Id x Side x Quantity x Price
+```
+
+where
+
+```
+Id = Natural number ({1, 2, 3, ...})
+Side = {Buy, Sell}
+Quantity = Natural number
+Price = Integers in the range (-1_000_000, 1_000_000)
+```
+
+An `OrderBook` contains a set of orders.
+
+
+## Orders from Venue
+
+Orders are received from a venue in the form of a stream of delta events.
+The events delta are defined as follows
+
+```haskell
+-- Create a new order
+createOrder (Side, Quantity, Price)
+
+-- Update an existing order's price
+updateOrder (Id, Price)
+
+-- Remove an existing order
+removeOrder (Id)
+```
+
+Implement operations handling these events:
+
+```haskell
+-- Create a new order and return its Id
+createOrder :: OrderBook -> (Side, Quantity, Price) -> (OrderBook, Id)
+
+-- Update an existing order's price and returns the updated order
+updateOrder :: OrderBook -> (Id, Price) -> (OrderBook, Order)
+
+-- Remove an existing order and return the removed order
+removeOrder :: OrderBook -> Id -> (OrderBook, Order)
+```
+
+Implement functions
+
+```haskell
+bids :: OrderBook -> List[Order]
+asks :: OrderBook -> List[Order]
+```
+
+where `bids` returns the list of buy orders and `asks` returns the list of sell orders. The orders should be sorted by price in descending order for bids and ascending order for asks.
+
---
+++ simulator/simulate.py
@@ -0,0 +1,128 @@
+import argparse
+import random
+from dataclasses import dataclass
+
+
+@dataclass
+class SimulatorState:
+    """State of the simulator."""
+
+    largest_id: int = 0
+
+
+@dataclass
+class CreateOrder:
+    """Create order message."""
+
+    side: str
+    quantity: int
+    price: float
+
+    def __str__(self):
+        return f"CREATE {self.side} {self.quantity} {self.price}"
+
+
+@dataclass
+class UpdateOrder:
+    """Update order message."""
+
+    order_id: int
+    price: float
+
+    def __str__(self):
+        return f"UPDATE {self.order_id} {self.price}"
+
+
+@dataclass
+class RemoveOrder:
+    """Remove order message."""
+
+    order_id: int
+
+    def __str__(self):
+        return f"REMOVE {self.order_id}"
+
+
+Event = CreateOrder | UpdateOrder | RemoveOrder
+
+
+def sample_side(state: SimulatorState) -> str:
+    """Sample a side for an order."""
+    return ["Buy", "Sell"][random.choice([0, 1])]
+
+
+def sample_quantity(state: SimulatorState) -> int:
+    """Sample a quantity for an order."""
+    return random.randint(1, 1000000)
+
+
+def sample_price(state: SimulatorState) -> float:
+    """Sample a price for an order."""
+    sign = random.choice([-1, 1])
+    price = random.randint(1, 1000000)
+    return sign * price
+
+
+def sample_new_order(state: SimulatorState) -> CreateOrder:
+    """Sample a new order."""
+    side = sample_side(SimulatorState())
+    quantity = sample_quantity(SimulatorState())
+    price = sample_price(SimulatorState())
+    state.largest_id += 1
+    return CreateOrder(side, quantity, price)
+
+
+def sample_update_order(state: SimulatorState) -> UpdateOrder:
+    """Sample an update order."""
+    order_id = random.randint(0, state.largest_id)
+    price = sample_price(state)
+    return UpdateOrder(order_id, price)
+
+
+def sample_remove_order(state: SimulatorState) -> RemoveOrder:
+    """Sample a remove order."""
+    order_id = random.randint(0, state.largest_id)
+    return RemoveOrder(order_id)
+
+
+def sample_event(state: SimulatorState) -> Event:
+    """Sample an event."""
+    event_type = random.choice([CreateOrder, UpdateOrder, RemoveOrder])
+    if event_type == CreateOrder:
+        return sample_new_order(state)
+    elif event_type == UpdateOrder:
+        return sample_update_order(state)
+    else:
+        return sample_remove_order(state)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simulates a sequence of order events.",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-updates",
+        type=int,
+        default=100,
+        help="Number of updates to simulate",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=argparse.FileType("w"),
+        metavar="FILE",
+        nargs="?",
+        default="-",
+        help="Output file for events",
+    )
+    args = parser.parse_args()
+
+    state = SimulatorState()
+    for _ in range(args.num_updates):
+        event = sample_event(state)
+        print(event)
+
+
+if __name__ == "__main__":
+    main()
